438e8c8a31e022cd844c1379ac4c16175051dee5
jonikumardas <jonikumardas213@gmail.com>
2022-06-10 22:04:20 +0600

    type effect

---
+++ index.js
@@ -0,0 +1,16 @@
+
+import Typewriter from 'typewriter-effect';
+
+<Typewriter
+    options={{
+        autoStart: true,
+        loop: true,
+        delay: 40,
+        strings: [
+            "HI ",
+            " This is Joni kumar das ",
+            " Iam a professional web Developer",
+            " Iam  a full stack Web Developer "
+        ]
+    }}
+/>
